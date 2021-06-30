b366ab77cdce05834ccc7ce544c139b006dad94b
[vedhashni] <[vedhavel.1234@gmail.com]>
2021-06-30 19:21:33 +0530

    [Vedhashni] updated the file with Dice

--- Snake_ladder/Program.cs
+++ Snake_ladder/Program.cs
@@ -4,6 +4,7 @@ namespace Snake_ladder
 {
    /// <summary>
    /// UC1 - initializing the single player at start position "0"
+   /// UC2 - Using Random dice values are computed
    /// </summary>
     class Program
     {
@@ -13,6 +14,20 @@ namespace Snake_ladder
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Snake Ladder Problem!");
+            Program.Firstplayer();
+        }
+
+        // Function to compute firstplayer's dice value
+        public static void Firstplayer()
+        {
+            Console.WriteLine(Program.Dicerolled());
+        }
+
+        // Function to compute dice values randomly
+        public static int Dicerolled()
+        {
+            Random random = new Random();
+            return random.Next(1, 7);
         }
     }
 }
