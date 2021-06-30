c5d4df999757fdccfdbcac70eda6f28fbce3235c
[vedhashni] <[vedhavel.1234@gmail.com]>
2021-06-30 19:11:01 +0530

    [Vedhashni] updated the file with single player at 0th position

--- Snake_ladder/Program.cs
+++ Snake_ladder/Program.cs
@@ -2,8 +2,14 @@
 
 namespace Snake_ladder
 {
+   /// <summary>
+   /// UC1 - initializing the single player at start position "0"
+   /// </summary>
     class Program
     {
+        public const int start_position = 0;
+        public const int board_size = 100;
+        public static int player1Position = start_position;
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Snake Ladder Problem!");
